8544370989f65306d4c4fa9a8843cfd23949aeb9
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-15 15:25:50 -0300

    Add project files.

---
+++ Fila/Program.cs
@@ -0,0 +1,9 @@
+﻿using Fila;
+using System.Reflection.PortableExecutable;
+
+FilaClass f = new FilaClass();
+Pessoa pessoa = new Pessoa();
+
+Console.WriteLine("Digite o nome da pessoa que deseja adicionar:");
+pessoa.Nome = Console.ReadLine()!;
+
