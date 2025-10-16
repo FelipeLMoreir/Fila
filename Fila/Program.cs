6ddeace1c0e5044b502b8540c4387b24efb75fef
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-16 00:00:39 -0300

    atualizado

--- Fila/Program.cs
+++ Fila/Program.cs
@@ -1,30 +1,35 @@
 ﻿using Fila;
 using System.Reflection.PortableExecutable;
 
-
-Console.WriteLine("Escolha uma opção:\n1 - Inserir Pessoa\n2 - Remover Pessoa" +
-    "\n3 - Tamanho da Fila\n4 - Exibir a Fila");
-int opcao = int.Parse(Console.ReadLine()!);
-
+int opcao;
 FilaClass f = new FilaClass();
 
-switch (opcao)
+do
 {
-    case 1:
-        Console.WriteLine("Digite o nome da pessoa que deseja adicionar:");
-        string nome = Console.ReadLine()!;
-        f.InserirPessoa(new Pessoa { Nome = nome });
-        break;
-    case 2:
-        f.RemoverPessoa();
-        break;
-    case 3:
-        f.TamanhoFila();
-        break;
-    case 4:
-        f.ImprimirFila();
-        break;
-    default:
-        break;
-}
 
+    Console.WriteLine("Escolha uma opção:\n1 - Inserir Pessoa\n2 - Remover Pessoa" +
+        "\n3 - Tamanho da Fila\n4 - Exibir a Fila");
+    opcao = int.Parse(Console.ReadLine()!);
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Digite o nome da pessoa que deseja adicionar:");
+            string nome = Console.ReadLine()!;
+            f.InserirPessoa(new Pessoa { Nome = nome });
+            Console.Clear();
+            break;
+        case 2:
+            f.RemoverPessoa();
+            break;
+        case 3:
+            f.TamanhoFila();
+            break;
+        case 4:
+            f.ImprimirFila();
+            break;
+        default:
+            break;
+    }
+    
+} while (opcao != 0);
