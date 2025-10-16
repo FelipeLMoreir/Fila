829febd55126b499bc561e3f2c076b526c9924dd
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-16 01:04:20 -0300

    att

---
+++ FilaBanco/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaBanco
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public bool EhPrioritario { get; set; }
+
+        public Cliente(string nome, int idade, bool ehPrioritario)
+        {
+            Nome = nome;
+            Idade = idade;
+            EhPrioritario = ehPrioritario;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} (Idade: {Idade}, {(EhPrioritario ? "Prioritário" : 
+                "Normal")})";
+        }
+    }
+}
